0be64797109531b0680121f10d5fe452bddd6a82
apurbadh <erikduke182@gmail.com>
2021-10-13 11:46:54 +0545

    input checking done

--- main.go
+++ main.go
@@ -2,8 +2,29 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
+	checkInput()
 	fmt.Println("App started")
+}
+
+func checkInput() {
+	if len(os.Args) == 1 {
+		fmt.Println("Not Enough Arguments")
+		os.Exit(1)
+		return
+	} else if len(os.Args) == 2 && os.Args[1] != "help" {
+		fmt.Println("Invalid arguments")
+		os.Exit(1)
+		return
+	} else if os.Args[1] != "help" && os.Args[1] != "listen" && os.Args[1] != "emit" {
+		fmt.Println("Invalid arguments")
+		os.Exit(1)
+	} else if (os.Args[1] == "listen" || os.Args[1] == "emit") && len(os.Args) == 2 {
+		fmt.Println("Not Enough Arguments")
+		os.Exit(1)
+	}
+
 }
