cda80ba52c4d00e14cbcb0f19b773fd8af816dfb
apurbadh <erikduke182@gmail.com>
2021-10-13 15:48:07 +0545

    function and role division created

--- main.go
+++ main.go
@@ -3,14 +3,22 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 )
 
 func main() {
-	checkInput()
-	fmt.Println("App started")
+	checkInvalidInput()
+
+	if os.Args[1] == "help" {
+		help()
+	} else if os.Args[1] == "listen" {
+		listen()
+	} else if os.Args[1] == "emit" {
+		emit()
+	}
 }
 
-func checkInput() {
+func checkInvalidInput() {
 	if len(os.Args) == 1 {
 		fmt.Println("Not Enough Arguments")
 		os.Exit(1)
@@ -25,6 +33,23 @@ func checkInput() {
 	} else if (os.Args[1] == "listen" || os.Args[1] == "emit") && len(os.Args) == 2 {
 		fmt.Println("Not Enough Arguments")
 		os.Exit(1)
+	} else if os.Args[1] == "listen" {
+		_, err := strconv.ParseInt(os.Args[2], 0, 64)
+		if err != nil {
+			fmt.Println("Invalid Port")
+			os.Exit(1)
+		}
 	}
 
+}
+
+func help() {
+	fmt.Println("Help Menu")
+	fmt.Println("help : Opens help menu")
+	fmt.Println("emit <ip> : Tries to get the revershell in the IP")
+	fmt.Println("listen <port> : Listen for any connection in the port")
+	fmt.Println()
+	fmt.Println("Examples : ")
+	fmt.Println("./reverseshell listen 8080")
+	fmt.Println("./reverseshell emit 192.168.100.244")
 }
